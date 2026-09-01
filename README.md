ff2c8eb134c7f93b90a080046443312ac4132516
Tartt <jeredwiley@gmail.com>
2026-08-31 23:16:00 -0500

    V2, added various new features

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # PM:TTYD Great Tree AMW Coordinate Finder
-Finds valid floats that write zeroes to an inputted address when setting up Arbitrary Memory Write in Chapter 2.
+Finds valid positions that write zeroes to an inputted address when setting up Arbitrary Memory Write in Chapter 2.
+
+This tool is built to get results from `SoundEfxStop` AMW; there are other functions that can theoretically provide different AMW results, but practical uses for them have yet to be found.
 ## Building
 `gcc -Ofast -o amw -I ./include amw.c`
 ## Usage
-`amw [addr]`
-examples: `amw 0xC0305BE8` `amw C0305BE8`
+`amw [region] [addr] (NO_EPSILON)`
+examples: `amw JP 0x80305BE8` `amw EU1 8042CB10 NO_EPSILON`
 
-Writing to an address in uncached memory (0xC0000000 - 0xC17FFFFF) will write 8 bytes of 00.
+The program will automatically check for addresses in both cached and uncached memory when provided an address in the 0x8XXXXXXX range.
+
+Writing to an address in uncached memory (0xC0000000 - 0xC17FFFFF) will write 8 bytes of 00; otherwise it will write 2 bytes of 00.
+
+The program automatically excludes position results that are too close to 0; appending `NO_EPSILON` to the input will include these results in the output.
+
+Results lower than -1000 or higher than 1000 are excluded unconditionally.
 ## G*nerative AI Disclosure
-I did not use g*n AI in any way to create this very simple C program
+I did not use g*n AI in any way to create this C program.
 ## Credits
 * [SeekyCt/spm-headers](https://github.com/SeekyCt/spm-headers) for the included common.h, used for typedef mnemonics
-* [Zephiles](https://github.com/Zephiles/) for the math used in get_addr_from_coord()
+* [Zephiles](https://github.com/Zephiles/) for the math used in get_addr_from_coord() & for guidance on how AMW works
 * Various Paper Mario: The Thousand-Year Door reverse engineers for discovering AMW and finding uses of it in speedruns
--- amw.c
+++ amw.c
@@ -1,42 +1,93 @@
 #include <common.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*
-    Program to find working floats that write to a desired address in Great Tree AMW
-    Exclusively supports the JP version of the game at this time
-    Usage: amw [address]
-    Address should be in the 0xC0000000 range for an 8-byte write
-    Outputs matching floats in hexadecimal format
+    Program to find working floats that write to a desired address in AMW
+    Usage: amw [version] [address] (NO_EPSILON)
+    Valid version inputs are JP, US, EU1, EU2 (see readme for explanation of EU1 and EU2)
+    Input an address in the 0x80000000 range and it will check for writes to both cached and uncached memory
 */
 
-// Rough, generous estimates
-#define ZPOS_MIN 0x43250000
-#define ZPOS_MAX 0xC3090000
+typedef union {
+    u32 u;
+    f32 f;
+} Coord;
+
+s32 bytesWritten[3] = {2, 8, 8};
+
+const char * versionNames[4] = {"JP", "US", "EU1", "EU2"};
+
+typedef enum : s32 {
+    JP,
+    US,
+    EU1,
+    EU2
+} Region;
+
+#define ZPOS_MIN -2000.0f
+#define ZPOS_MAX 2000.0f
+#define EPSILON 0.00001f
 
 // based on math from Zephiles
-u32 get_addr_from_coord(u32 coord) {
-    return (0x806E0640 + ((coord * 0x88) & 0xFFFFFFFF)) & 0xFFFFFFFF;
+u32 get_addr_from_coord(Region region, u32 coord) {
+    switch (region) {
+    case JP:
+        return (0x806E0640 + ((coord * 0x88) & 0xFFFFFFFF)) & 0xFFFFFFFF;
+        break;
+    case US:
+        return (0x806EED40 + ((coord * 0x88) & 0xFFFFFFFF)) & 0xFFFFFFFF;
+        break;
+    case EU1:
+        return (0x8072FC60 + ((coord * 0x88) & 0xFFFFFFFF)) & 0xFFFFFFFF;
+        break;
+    case EU2:
+        return (0x806FB860 + ((coord * 0x88) & 0xFFFFFFFF)) & 0xFFFFFFFF;
+        break;
+    }
+}
+
+Region get_region(const char * name) {
+    for (s32 i = 0; i < 4; i += 1) {
+        if (strcmp(name, versionNames[i]) == 0)
+            return (Region)i;
+    }
+    printf("Error parsing region input\n");
+    abort();
 }
 
 s32 main(s32 argc, char * argv[]) {
-    if (argc < 2) {
-        printf("please input an address\n");
+    if (argc < 3) {
+        printf("Please input a region and an address\n");
         return 0;
     }
-    u32 desiredAddr;
-    if (sscanf(argv[1], "%x", &desiredAddr) != 1) {
-        printf("error parsing address\n");
+    Region region = get_region(argv[1]);
+    u32 baseAddr;
+    if (sscanf(argv[2], "%x", &baseAddr) != 1) {
+        printf("Error parsing address input\n");
         return 0;
     }
-    u32 curCoord = ZPOS_MIN;
+    bool chkEpsilon = true;
+    if (argc >= 4)
+        chkEpsilon = strcmp(argv[3], "NO_EPSILON");
+    u32 addr[3] = {baseAddr, baseAddr + 0x40000000, baseAddr + 0x3FFFFFFC};
+    Coord curCoord;
+    curCoord.u = 0;
     s32 matches = 0;
     do {
-        if (desiredAddr == get_addr_from_coord(curCoord)) {
-            printf("0x%X\n", curCoord);
-            matches += 1;
+        for (s32 i = 0; i < 3; i += 1) {
+            if (curCoord.f >= ZPOS_MIN && curCoord.f <= ZPOS_MAX) {
+                if (!chkEpsilon || (curCoord.f >= EPSILON || curCoord.f <= -EPSILON)) {
+                    if (addr[i] == get_addr_from_coord(region, curCoord.u)) {
+                        printf("Coord 0x%X (%.8f) writes %d bytes to addr 0x%X\n", curCoord.u, curCoord.f, bytesWritten[i], addr[i]);
+                        matches += 1;
+                    }
+                }
+            }
         }
-        curCoord += 1;
-    } while (curCoord < ZPOS_MAX);
-    printf("found %d matches for address 0x%X\n", matches, desiredAddr);
+        curCoord.u += 1;
+    } while (curCoord.u != 0xFFFFFFFF);
+    printf("Found %d matches for base address 0x%X\n", matches, baseAddr);
     return 0;
 }
